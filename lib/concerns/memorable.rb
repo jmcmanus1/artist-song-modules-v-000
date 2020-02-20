16939820ef298e0f68d55e4e895dc451a25f79cf
Julie  McManus <julieannesays@gmail.com>
2020-02-20 22:41:58 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -9,8 +9,9 @@ module Memorable
     end
   end
 
-module InstanceMethods
-  def initialize
-    self.class.all << self
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
   end
 end
