b9e3cd22bf613b9652e4a7ca9c0d984f5213dff5
Julie  McManus <julieannesays@gmail.com>
2020-02-20 22:49:08 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,6 +1,7 @@
 module Findable
-
-  def find_by_name(name)
-    all.detect {|x| x.name == name}
+  module ClassMethods
+    def find_by_name(name)
+      all.detect {|x| x.name == name}
+    end
   end
 end
